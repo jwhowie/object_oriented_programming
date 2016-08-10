2cee02423ce39dc1eff1857c0448c6eeea2e2835
John Howie <jkhowie63@gmail.com>
2016-08-10 14:49:29 -0400

    made the cats meow

--- object_oriented_programming/cat.rb
+++ object_oriented_programming/cat.rb
@@ -1,20 +1,22 @@
 class Cat
-  attr_accessor :name, :preferred_food, :meal_time
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
 
-  public
   def eats_at
     meridiam = "AM"
-    hour = meal_time
-    if meal_time > 12
+    hour = @meal_time
+    if @meal_time > 12
       meridiam = "PM"
-      hour = meal_time - 12
+      hour = @meal_time - 12
     end
     return "#{hour} #{meridiam}"
+  end
+
+  def meow
+    return "My name is #{@name} and I eat #{@preferred_food} at #{eats_at}"
   end
 end
 
@@ -22,7 +24,5 @@ end
 dave = Cat.new("Dave", "Puriena", 11)
 socks = Cat.new("Socks", "Puriena", 18)
 
-puts dave.name
-puts dave.preferred_food
-puts dave.eats_at
-puts socks.eats_at
+puts dave.meow
+puts socks.meow
